61b0a311250ad7b68ea7e655079f0eca8498b4d4
Rishi Vejani <123580547+15Rishi@users.noreply.github.com>
2023-03-16 16:21:43 +0530

    Add files via upload

---
+++ USD_INR.c
@@ -0,0 +1,10 @@
+//To convert USD into INR
+#include<stdio.h>
+void main()
+{
+    float INR, USD;
+    printf("Enter USD to INR Converter -->\n");
+    printf("Enter USD: $");
+    scanf("%f",&USD);
+    printf("%f INR",USD * 82.5);
+}
